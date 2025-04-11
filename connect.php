50a639c8b1089c9570ba13d0c549639bf3289a9c
VISSHWA PM <72188494+Visshwa96@users.noreply.github.com>
2025-04-11 17:20:30 +0530

    connect.php

--- connect.php
+++ connect.php
@@ -4,3 +4,9 @@ $con = mysqli_connect("localhost","root","","gabbage");
 error_reporting(E_ALL^E_NOTICE);
 
  ?>
+<?php
+ 
+$con = mysqli_connect("localhost","root","","gabbbbbage");
+error_reporting(E_ALL^E_NOTICE);
+
+ ?>
